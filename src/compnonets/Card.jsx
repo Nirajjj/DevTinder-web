fee5e4e22079b130b372137584ed497be0d63162
Niraj Parab <nirajparab007@gmail.com>
2024-12-19 21:56:52 +0530

    add feature of interested/ignore user in feed component create removeUserFromFeed reducer in redux store to remove an user after status is set in feed

--- src/compnonets/Card.jsx
+++ src/compnonets/Card.jsx
@@ -1,11 +1,26 @@
 import React from "react";
-import { DEFAULT_AVATAR } from "../utils/constants";
+import { BASE_URL, DEFAULT_AVATAR } from "../utils/constants";
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import { removeUserFromFeed } from "../utils/feedSlice";
 
 const Card = ({ user }) => {
-  if (!user) return <div>loading</div>;
-  const { firstName, lastName, age, about, photoUrl, gender } = user;
+  const { _id, firstName, lastName, age, about, photoUrl, gender } = user;
   console.log(user);
-
+  const dispatch = useDispatch();
+  const handleSendRequest = async (staus, id) => {
+    try {
+      const res = await axios.post(
+        BASE_URL + "/request/" + staus + "/" + id,
+        {},
+        { withCredentials: true }
+      );
+      dispatch(removeUserFromFeed(id));
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  if (!user) return <div>loading</div>;
   return (
     <div className=" card-actions rounded-lg bg-base-300 w-3/12  shadow-xl">
       <figure>
@@ -16,8 +31,22 @@ const Card = ({ user }) => {
         {age && gender && <p>{age + " " + gender}</p>}
         <p>{about}</p>
         <div className="card-actions justify-center">
-          <button className="btn btn-info">ignore</button>
-          <button className="btn btn-success">Interested</button>
+          <button
+            className="btn btn-info"
+            onClick={() => {
+              handleSendRequest("ignored", _id);
+            }}
+          >
+            ignore
+          </button>
+          <button
+            className="btn btn-success"
+            onClick={() => {
+              handleSendRequest("interested", _id);
+            }}
+          >
+            Interested
+          </button>
         </div>
       </div>
     </div>
--- src/compnonets/Feed.jsx
+++ src/compnonets/Feed.jsx
@@ -17,7 +17,7 @@ const Feed = () => {
         withCredentials: true,
       });
 
-      dispatch(addFeed(res.data));
+      dispatch(addFeed(res.data.feedCards));
     } catch (error) {
       if (error?.response?.status === 401) {
         Navigate("/login");
@@ -25,20 +25,22 @@ const Feed = () => {
       console.error(error);
     }
   };
-  useEffect(() => {
-    // const fetchData = async () => {
-    //   await
-    postFeed();
-    // };
-    // fetchData();
-  }, []);
-
-  return feed ? (
-    <div className=" py-7 flex justify-center items-center">
-      <Card user={feed.feedCards[0]} />
+  // useEffect(() => {
+  //   // const fetchData = async () => {
+  //   //   await
+  //   // };
+  //   // fetchData();
+  // }, []);
+  postFeed();
+  if (!feed || feed?.length <= 0) {
+    return (
+      <h2 className="font-semibold text-lg text-center h-screen">No feed</h2>
+    );
+  }
+  return (
+    <div className=" py-7 flex justify-center items-center h-screen">
+      <Card user={feed[0]} />
     </div>
-  ) : (
-    <div>no feed</div>
   );
   // :
   // (
