9e040ddab8c7328285f5afd9247e99368a227aba
Niraj Parab <nirajparab007@gmail.com>
2024-12-21 16:17:25 +0530

    change the base url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,5 @@
-const BASE_URL = "http://localhost:7777";
+// const BASE_URL = "http://localhost:7777";
+const BASE_URL = "https://devtinder-o67m.onrender.com";
 const DEFAULT_AVATAR =
   "https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp";
 
