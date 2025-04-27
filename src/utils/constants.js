4f7cf0cabe9e45ab60c14745ceac6440e2571c1e
Niraj Parab <nirajparab007@gmail.com>
2025-04-27 19:43:27 +0530

    change the base_url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,9 +1,11 @@
 // const BASE_URL = "http://localhost:7777";
 // const BASE_URL = "https://devtinder-o67m.onrender.com";
-const BASE_URL =
-  import.meta.env.VITE_NODE_ENV === "production"
-    ? "https://devtinder-458109.el.r.appspot.com/"
-    : "http://localhost:7777";
+const BASE_URL = "https://devtinder-458109.el.r.appspot.com/";
+
+// const BASE_URL =
+//   import.meta.env.VITE_NODE_ENV === "production"
+//     ? "https://devtinder-458109.el.r.appspot.com/"
+//     : "http://localhost:7777";
 const DEFAULT_AVATAR =
   "https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp";
 
